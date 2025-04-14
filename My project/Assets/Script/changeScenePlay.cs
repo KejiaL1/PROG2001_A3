d2c5feae1bb104d117fac5bca896c8f4c90f4c47
ShiminJin <s.jin.11@student.scu.edu.au>
2025-04-14 17:42:29 +0800

    Further refinement of the UI
    Added Back and Restart buttons to improve the UI format.

---
+++ My project/Assets/Script/changeScenePlay.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScenePlay : MonoBehaviour
+{
+    // Load scene by build index
+    public void LoadMyScene(int sceneNumber)
+    {
+
+        StartCoroutine(PlayMySound(sceneNumber)); // Load scene by name
+        //SceneManager.LoadScene(sceneNumber);
+    }
+
+    // Delayed load for int parameter
+    public void LoadMyScene(string sceneName)
+    {
+        StartCoroutine(PlayMySound(sceneName)); 
+    }
+
+    // Immediate load for string parameter
+    private IEnumerator PlayMySound(int sceneNumber)
+    {
+        yield return new WaitForSeconds(0.5f); 
+        SceneManager.LoadScene(sceneNumber); 
+    }
+
+    private IEnumerator PlayMySound(string sceneName)
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
